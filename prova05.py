66761dfb6d2aaaf6cf644fc3516feb282fdea7fa
Ricardo Altmann <tenaltmann@gmail.com>
2025-02-03 16:07:08 -0300

    Criação das opções do menu

--- prova05.py
+++ prova05.py
@@ -2,20 +2,23 @@ numero_de_alunos = int(input("Inisira o número de alunos da sua sala: "))
 alunos = {}
 
 
-while True:
-    print("Opções: ")
-    print("1 - Adicionar Aluno")
-    print("2 - Sair")
-
-    opcao = input("Digite o Número da opção: ")
+for i in range (numero_de_alunos):
+    
+    opcao = int(input("Deseja adicionar um aluno: "))
 
     if opcao == 1:
-        nome = input("Nome do aluno: ")
         
+        nome = input("Nome do aluno: ")
         nota_1 = float(input("Insira a primeira nota do aluno: "))
         nota_2 = float(input("Insira a primeira nota do aluno: "))
         nota_3 = float(input("Insira a primeira nota do aluno: "))
-
         nota_lista = [nota_1, nota_2, nota_3]
-
         alunos[nome] = nota_lista
+
+    elif opcao == 2:
+        break
+
+print(alunos)
+
+
+

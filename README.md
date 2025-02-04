633ed1489c72b4fa691eb024fae86f611b76d0df
Ricardo Altmann <tenaltmann@gmail.com>
2025-02-04 10:55:26 -0300

    Resolução da prova 05

---
+++ README.md
@@ -0,0 +1,9 @@
+Resolução da Prova do aluno: Ricardo Soares de Moura Altmann Filho 20/10/2002 LPIA | Lógica da Programação com ChatGPT
+
+Prova iNTRODUÇÃO A INTELIGÊNCIA ARTIFICIAL do módulo Aula05
+
+Desenvolva um programa em Python para calcular a média de notas de alunos em uma disciplina. O programa deve solicitar ao usuário o número de alunos e, em seguida, para cada aluno, pedir o nome e três notas. Utilize um loop 'for' para iterar sobre os alunos e suas notas.
+
+Além disso, implemente uma estrutura condicional para verificar se cada aluno foi aprovado ou reprovado, considerando que a média mínima para aprovação é 7.0. Exiba o nome do aluno, suas notas, média e a indicação de aprovação ou reprovação.
+
+Ao final, exiba a média geral da turma.
--- prova05.py
+++ prova05.py
@@ -1,24 +1,54 @@
-numero_de_alunos = int(input("Inisira o número de alunos da sua sala: "))
+numero_de_alunos = int(input("Inisira o número de alunos que voc~e quer cadatrar: "))
 alunos = {}
-
+soma_total = 0
+quantidade_de_notas = 0
 
 for i in range (numero_de_alunos):
+
+    nome = input("Nome do aluno: ")
+    notas = [
+        float(input("Insira a primeira nota do aluno: ")),
+        float(input("Insira a segunda nota do aluno: ")),
+        float(input("Insira a terceira nota do aluno: "))]
     
-    opcao = int(input("Deseja adicionar um aluno: "))
+    alunos[nome] = notas
 
-    if opcao == 1:
-        
-        nome = input("Nome do aluno: ")
-        nota_1 = float(input("Insira a primeira nota do aluno: "))
-        nota_2 = float(input("Insira a primeira nota do aluno: "))
-        nota_3 = float(input("Insira a primeira nota do aluno: "))
-        nota_lista = [nota_1, nota_2, nota_3]
-        alunos[nome] = nota_lista
 
-    elif opcao == 2:
-        break
+for nome, notas in alunos.items():    
+    soma_notas = sum(notas)
+    soma_total += soma_notas
+    quantidade_de_notas += len(notas)
 
-print(alunos)
+    media = soma_notas / 3
+
+    if media >= 7:
+        mencao = "APROVADO"
+    else:
+        mencao = "REPROVADO"
+    print(f"o aluno(a) {nome} tirou as seguintes {notas} e sua média foi {media:.2f}, portanto ele está {mencao}")
+
+
+media_sala = soma_total / quantidade_de_notas
+
+print(f" A MEDIA DA TURMA FOI DE {media_sala} PONTOS")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
